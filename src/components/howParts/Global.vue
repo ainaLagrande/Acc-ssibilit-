733ac2814d50d7ca415e5276a095b85647aa5413
Aïna Akagadzhieva <aakagadzhieva@groupe.zzroot.com>
2023-03-24 18:21:28 +0100

    function onclick

--- src/components/howParts/Global.vue
+++ src/components/howParts/Global.vue
@@ -1,49 +1,39 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import Accordion from 'primevue/accordion';
 import AccordionTab from 'primevue/accordiontab';
 
-// const props = defineProps({
-//     dataChapitreId: {
-//       type: String,
-//       required: true
-//     }
-// })
-
-// let elements = document.querySelectorAll('.test');
-
-// elements.forEach(element => {
-//   element.addEventListener('click', function() {
-//     // let chapitreId = element.getAttribute('dataChapitreId');
-//     // localStorage.setItem('chapitreId', 1);
-//     console.log("dlk");
-
-//   });
-// });
-
-const accordionTabs = document.querySelectorAll('test');
-
-accordionTabs.forEach(accordionTab => {
-  accordionTab.addEventListener('click', maFonction);
-});
+const accordionTabs = ref(null);
 
 function maFonction() {
   console.log("dlk");
   // Votre code à exécuter lorsque l'utilisateur clique sur l'élément
 }
 
-
+onMounted(() => {
+  if (accordionTabs.value) {
+    accordionTabs.value.querySelectorAll('.how-img').forEach((accordionTab) => {
+      accordionTab.addEventListener('click', maFonction);
+    });
+  }
+});
 </script>
-
 <template>
   <div class="how-parts-global">
     <div class="how-parts-box">
       <div class="how-parts-item">
         <Accordion >
-          <div id="test" class="test" dataChapitreId="1">
+          <div id="test" class="test" dataChapitreId="1" ref="accordionTabs">
             <AccordionTab  :header="'Fournir un titre unique pour chaque page ou vue.'" >
               <p>Cet élément du document est souvent la première information annoncée par la technologie d'assistance. Cela aide à dire aux gens quelle page ou vue ils vont commencer à naviguer.</p>
-              <img class="how-img" src="../../assets/images/title.png" alt="lang code">
+              <div class="how-img" @click="maFonction">
+                <img  src="../../assets/images/title.png" alt="lang code">
+              </div>
+
+              <div class="how-img" @click="maFonction">
+                <img  src="../../assets/images/title.png" alt="lang code">
+              </div>
+
             </AccordionTab>
           </div>
         </Accordion>
@@ -51,6 +41,8 @@ function maFonction() {
     </div>
   </div>
 </template>
+
+
 
 <style lang="scss" scoped>
 </style>
