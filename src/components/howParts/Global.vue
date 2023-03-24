2cd6f76fe12eb20420d4a6919631c1d02f0580bc
Aïna Akagadzhieva <aakagadzhieva@groupe.zzroot.com>
2023-03-24 18:41:20 +0100

    img zoom in progress

--- src/components/howParts/Global.vue
+++ src/components/howParts/Global.vue
@@ -2,17 +2,26 @@
 import { ref, onMounted } from 'vue';
 import Accordion from 'primevue/accordion';
 import AccordionTab from 'primevue/accordiontab';
-
 const accordionTabs = ref(null);
 
-function maFonction() {
-  console.log("dlk");
-  // Votre code à exécuter lorsque l'utilisateur clique sur l'élément
-}
 
+function maFonction() {
+  let images = document.querySelectorAll('.how-img-container');
+  
+  images.forEach((image) => {
+    image.addEventListener('click', () => {
+      if (image.classList.contains('zoomed')) {
+        image.classList.remove('zoomed');
+      } else {
+        image.classList.add('zoomed');
+      }
+    });
+  });
+  
+}
 onMounted(() => {
   if (accordionTabs.value) {
-    accordionTabs.value.querySelectorAll('.how-img').forEach((accordionTab) => {
+    accordionTabs.value.querySelectorAll('.how-img-container').forEach((accordionTab) => {
       accordionTab.addEventListener('click', maFonction);
     });
   }
@@ -26,12 +35,12 @@ onMounted(() => {
           <div id="test" class="test" dataChapitreId="1" ref="accordionTabs">
             <AccordionTab  :header="'Fournir un titre unique pour chaque page ou vue.'" >
               <p>Cet élément du document est souvent la première information annoncée par la technologie d'assistance. Cela aide à dire aux gens quelle page ou vue ils vont commencer à naviguer.</p>
-              <div class="how-img" @click="maFonction">
-                <img  src="../../assets/images/title.png" alt="lang code">
+              <div class="how-img-container" @click="maFonction">
+                <img class="how-img" src="../../assets/images/title.png" alt="lang code">
               </div>
 
-              <div class="how-img" @click="maFonction">
-                <img  src="../../assets/images/title.png" alt="lang code">
+              <div class="how-img-container" @click="maFonction">
+                <img class="how-img"  src="../../assets/images/title.png" alt="lang code">
               </div>
 
             </AccordionTab>
@@ -45,4 +54,8 @@ onMounted(() => {
 
 
 <style lang="scss" scoped>
+
+.zoomed {
+  transform: scale(2); /* ajuster la valeur de zoom selon vos besoins */
+}
 </style>
